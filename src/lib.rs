bd8f1463fb020e90b7de4d4d1f7ad8c88278b7b6
skogseth <herman.skogseth@me.com>
2022-11-17 21:15:07 +0100

    Added save functioN

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,39 @@
 #![allow(dead_code)]
 use std::path::Path;
-// use serde::Serialize;
+use serde::Serialize;
 
 #[derive(Debug)]
 pub struct Dataset<Iter: Iterator<Item = Data>, Data> {
     iterator: Iter,
 }
 
-impl<IntoIt, It, Data> From<IntoIt> for Dataset<It, Data>
+impl<IntoIter, Iter, Data> From<IntoIter> for Dataset<Iter, Data>
 where
-    IntoIt: IntoIterator<Item = Data, IntoIter = It>,
-    It: Iterator<Item = Data>,
+    IntoIter: IntoIterator<Item = Data, IntoIter = Iter>,
+    Iter: Iterator<Item = Data>,
 {
-    fn from(into_it: IntoIt) -> Self {
-        Dataset { iterator: into_it.into_iter() }
+    fn from(into_iterator: IntoIter) -> Self {
+        Dataset { iterator: into_iterator.into_iter() }
     }
 }
 
-/*
-impl<Iter: Iterator<Item = Data>, Data: Serialize> Dataset<Iter, Data> {
-    fn save(self, filename: &Path) {
-        // save that data to csv using csv writer
+// Setialize functions
+impl<Iter: Iterator<Item = Data>, Data: Serialize + std::fmt::Debug> Dataset<Iter, Data> {
+    fn save(self, filepath: &Path) -> Result<(), std::io::Error> {
+        let mut writer = csv::Writer::from_path(filepath)?;
+        log::debug!("Writing to file: {:?}", filepath);
+        for datapoint in self.iterator {
+            log::debug!("Writing datapoint: {:?}", datapoint);
+            writer.serialize(datapoint)?;
+            log::debug!("Succesfull!");
+        }
+        log::debug!("Finished writing data, flushing");
+        writer.flush()?;
+        log::debug!("Write succesfull!");
+        Ok(())
     }
 }
-*/
+
 
 #[cfg(test)]
 mod tests {
@@ -41,6 +51,25 @@ mod tests {
         let vector = vec![1, 2, 3];
         let dataset = Dataset::from(vector);
         println!("{:?}", dataset);
+    }
+    
+    #[derive(Serialize)]
+    #[derive(Debug)]
+    struct Count {
+        character: char,
+        count: u32,
+    }
+    
+    #[test]
+    fn save_to_file() {
+        let chars = vec!['h', 'l', 'j'];
+        let numbers = vec![115, 83, 24];
+        let zipped = std::iter::zip(chars, numbers);
+        let data = zipped.map(|x| Count { character: x.0, count: x.1 } );
+        let dataset = Dataset::from(data);
+        let path = std::path::PathBuf::from("test.csv");
+        let result = dataset.save(&path);
+        result.unwrap();
     }
 }
 

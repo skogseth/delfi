487244fe964ee35d30a8ea746991c010c52adfeb
skogseth <herman.skogseth@me.com>
2022-12-28 12:51:33 +0100

    Added crates: delfi-derive & delfi-test

---
+++ crates/delfi-test/src/main.rs
@@ -0,0 +1,24 @@
+use delfi::Datapoint;
+use delfi_derive::Datapoint;
+
+#[derive(Debug, Datapoint)]
+struct Point(f64, f64);
+
+#[derive(Debug, Datapoint)]
+struct Count {
+    ch: char,
+    i: usize,
+}
+
+fn main() {
+    let c = Count { ch: 'a', i: 32 };
+    println!("{c:#?}");
+    let r = c.record();
+    println!("{:?}", r);
+
+    let p = Point(3.0, 2.0);
+    println!("{p:#?}");
+    let r = p.record();
+    println!("{:?}", r);
+}
+
